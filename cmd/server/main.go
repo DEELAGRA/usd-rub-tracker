44f30f5b850262775ab0e75e28ca8fa3124da430
Ilya Evgrafov <iaevgrafov@avito.ru>
2026-04-16 23:25:12 +0300

    Создал структуру директорий и запустил сервер

---
+++ cmd/server/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
+
+func main() {
+	r := chi.NewRouter()
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello world👋"))
+	})
+	r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(`{"status": "ok"}`))
+	})
+
+	fmt.Println("🚀 Сервер запущен на http://localhost:8080")
+	if err := http.ListenAndServe(":8080", r); err != nil {
+		fmt.Printf("Ошибка запуска сервера: %v", err)
+	}
+}
