bf42a5ab8067e5ee59b69db7a498dd0755ca9beb
DEELAGRA <deelagra@gmail.com>
2026-04-29 17:47:11 +0300

    отфоматировал код, убрал неточности

--- internal/db/db-update.go
+++ internal/db/db-update.go
@@ -2,16 +2,20 @@ package database
 
 import (
 	"context"
+	"fmt"
 	models "usd-rub-tracker/pkg/models"
 
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
-func SaveRate(ctx context.Context, pool *pgxpool.Pool, rates models.RateModels) error {
+func SaveRate(ctx context.Context, pool *pgxpool.Pool, rate models.RateModels) error {
 	sqlQuery := `
  	INSERT INTO rates(rate, date, created_at)
  	VALUES($1,$2,$3)
  	`
-	_, err := pool.Exec(ctx, sqlQuery, rates.Rate, rates.Date, rates.Created_at)
-	return err
+	_, err := pool.Exec(ctx, sqlQuery, rate.Rate, rate.Date, rate.CreatedAt)
+	if err != nil {
+		return fmt.Errorf("SaveRate: не удалось записать данные: %w", err)
+	}
+	return nil
 }
--- pkg/models/models.go
+++ pkg/models/models.go
@@ -3,8 +3,8 @@ package models
 import "time"
 
 type RateModels struct {
-	ID         int
-	Rate       float64
-	Date       time.Time
-	Created_at time.Time
+	ID        int
+	Rate      float64
+	Date      time.Time
+	CreatedAt time.Time
 }
